e24e596ca4f6dcd1791104512a189795d07c6d6f
hiepquocchung5-blip <hiepquocchung5@gmail.com>
2026-01-28 19:14:44 +0630

    middle_ware update withhtaccess

--- utils/auth_middleware.php
+++ utils/auth_middleware.php
@@ -3,6 +3,7 @@
 
 // 1. Handle CORS
 header("Access-Control-Allow-Origin: https://m.suropara.com");
+header("Access-Control-Allow-Credentials: true");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
