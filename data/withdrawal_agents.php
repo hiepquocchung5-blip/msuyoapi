e361f0c6a54f3c551b5ce6c07d8004a32431f6f0
hiepquocchung5-blip <hiepquocchung5@gmail.com>
2026-01-28 13:00:18 +0630

    update

--- data/withdrawal_agents.php
+++ data/withdrawal_agents.php
@@ -5,7 +5,7 @@
 ini_set('display_errors', 0);
 error_reporting(E_ALL);
 
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://m.api.suropara.com");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
--- data/withdrawal_banks.php
+++ data/withdrawal_banks.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/../utils/auth_middleware.php'; 
 
 // Allow CORS
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://m.api.suropara.com");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
