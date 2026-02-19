12eb014587bb524513fb1e4dd3ebaa208a0d5470
hiepquocchung5-blip <hiepquocchung5@gmail.com>
2026-02-19 13:43:07 +0630

    update config

--- config/db.php
+++ config/db.php
@@ -10,16 +10,16 @@ if (file_exists($envLoaderPath)) {
 
 // 2. Database Credentials
 // Priority: Environment Variable -> Hardcoded Fallback (Your Local Settings)
-$host = getenv('DB_HOST') ?: 'localhost';
-$db   = getenv('DB_NAME') ?: 'suropara_myanmar_777';
-$user = getenv('DB_USER') ?: 'mm_suro_usr';
+$host = getenv('DB_HOST');
+$db   = getenv('DB_NAME') ;
+$user = getenv('DB_USER');
 // Check specific false condition for password as it can be empty string
-$pass = getenv('DB_PASS') ?: 'QwXHS7VapIJ4C8$9';
+$pass = getenv('DB_PASS');
 $charset = 'utf8mb4';
 
 // 3. API URL Configuration
 // Used for constructing image paths for the frontend/finance portal
-$apiUrl = getenv('API_PUBLIC_URL') ?: 'https://api.suropara.com/';
+$apiUrl = getenv('API_PUBLIC_URL') ?: 'https://apisuro.online/';
 
 if (!defined('API_BASE_URL')) {
     define('API_BASE_URL', $apiUrl);
