7bf59fdf7808710caf59fdc63555036b2a3440ed
hiepquocchung5-blip <hiepquocchung5@gmail.com>
2026-02-26 17:17:59 +0630

    update header

--- game/spin.php
+++ game/spin.php
@@ -10,10 +10,7 @@
 // ============================================================================
 
 header("Access-Control-Allow-Origin: https://suropara.com");
-header("Access-Control-Allow-Credentials: true");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200); exit;
