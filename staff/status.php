a30bcc6b4b8f8e1d3e985cb3d69aff6b56908f20
hiepquocchung5-blip <hiepquocchung5@gmail.com>
2026-03-12 07:22:23 +0630

    updat CORS for staff

--- staff/status.php
+++ staff/status.php
@@ -5,7 +5,9 @@
 
 require_once __DIR__ . '/../config/db.php';
 
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://slofinace.website/dashboard.php"); // Restrict to Finance Portal domain
+header("Access-Control-Allow-Methods: POST, GET");
+header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
 
 // Check if Request comes from Finance Portal Session
