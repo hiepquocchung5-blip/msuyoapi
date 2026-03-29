7bfe7b4749357597d01915e1bc0720a980f44aa1
hiepquocchung5-blip <hiepquocchung5@gmail.com>
2026-03-29 10:51:47 +0630

    user actions dir fix

--- user/action.php
+++ user/action.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../../utils/admin_middleware.php'; 
+require_once __DIR__ . '/../utils/admin_middleware.php'; 
 
 // Only FINANCE or GOD can ban/reset
 $admin = authenticateAdmin($pdo, 'FINANCE');
